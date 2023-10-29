2823402787fb31962cfce5f565097c7a1bd02d17
alex-211 <114176944+alex-211@users.noreply.github.com>
2023-10-29 16:42:46 +0100

    First

---
+++ idraulico/idraulico/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace idraulico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NUDday = new System.Windows.Forms.NumericUpDown();
+            this.TXTservice = new System.Windows.Forms.TextBox();
+            this.TXTrevenue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TXTsrch = new System.Windows.Forms.TextBox();
+            this.BTadd = new System.Windows.Forms.Button();
+            this.LST1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDday)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Giorno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Intervento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Incasso ";
+            // 
+            // NUDday
+            // 
+            this.NUDday.Location = new System.Drawing.Point(15, 25);
+            this.NUDday.Name = "NUDday";
+            this.NUDday.Size = new System.Drawing.Size(120, 20);
+            this.NUDday.TabIndex = 3;
+            // 
+            // TXTservice
+            // 
+            this.TXTservice.Location = new System.Drawing.Point(151, 25);
+            this.TXTservice.Name = "TXTservice";
+            this.TXTservice.Size = new System.Drawing.Size(101, 20);
+            this.TXTservice.TabIndex = 4;
+            // 
+            // TXTrevenue
+            // 
+            this.TXTrevenue.Location = new System.Drawing.Point(263, 25);
+            this.TXTrevenue.Name = "TXTrevenue";
+            this.TXTrevenue.Size = new System.Drawing.Size(100, 20);
+            this.TXTrevenue.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cerca intervento per tipo:";
+            // 
+            // TXTsrch
+            // 
+            this.TXTsrch.Location = new System.Drawing.Point(144, 74);
+            this.TXTsrch.Name = "TXTsrch";
+            this.TXTsrch.Size = new System.Drawing.Size(300, 20);
+            this.TXTsrch.TabIndex = 7;
+            this.TXTsrch.TextChanged += new System.EventHandler(this.TXTsrch_TextChanged);
+            // 
+            // BTadd
+            // 
+            this.BTadd.Location = new System.Drawing.Point(369, 17);
+            this.BTadd.Name = "BTadd";
+            this.BTadd.Size = new System.Drawing.Size(75, 33);
+            this.BTadd.TabIndex = 8;
+            this.BTadd.Text = "Aggiungi";
+            this.BTadd.UseVisualStyleBackColor = true;
+            this.BTadd.Click += new System.EventHandler(this.BTadd_Click);
+            // 
+            // LST1
+            // 
+            this.LST1.FormattingEnabled = true;
+            this.LST1.Items.AddRange(new object[] {
+            "I tuoi interventi appariranno qui..."});
+            this.LST1.Location = new System.Drawing.Point(13, 104);
+            this.LST1.Name = "LST1";
+            this.LST1.Size = new System.Drawing.Size(431, 134);
+            this.LST1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 267);
+            this.Controls.Add(this.LST1);
+            this.Controls.Add(this.BTadd);
+            this.Controls.Add(this.TXTsrch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TXTrevenue);
+            this.Controls.Add(this.TXTservice);
+            this.Controls.Add(this.NUDday);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NUDday)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown NUDday;
+        private System.Windows.Forms.TextBox TXTservice;
+        private System.Windows.Forms.TextBox TXTrevenue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TXTsrch;
+        private System.Windows.Forms.Button BTadd;
+        private System.Windows.Forms.ListBox LST1;
+    }
+}
+
---
+++ idraulico/idraulico/Form1.cs
@@ -0,0 +1,102 @@
+﻿// Ignore Spelling: inserisci Corretti dati
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace idraulico
+{
+    public partial class Form1 : Form
+    {
+        public struct interventions
+        {
+            public int day;
+            public float revenue;
+            public string interventionType;
+        }
+
+        const int MAXV = 10;
+        int nv = 0;
+        interventions[] array = new interventions[MAXV];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTadd_Click(object sender, EventArgs e)
+        {
+            leggiDati();
+        }
+
+        private void TXTsrch_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public bool datiCorretti()
+        {
+            if (TXTrevenue.Text == "")
+            {
+                MessageBox.Show("L'incasso non è stato compilato");
+                return false;
+            }
+
+            if (TXTservice.Text == "")
+            {
+                MessageBox.Show("Il tipo di servizio non è stato compilato");
+                return false;
+            }
+
+            if (!(float.TryParse(TXTrevenue.Text, out float no)))
+            {
+                MessageBox.Show("L'incasso deve essere un numero");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void leggiDati()
+        {
+            if (datiCorretti() == false)
+            {
+                return;
+            }
+
+            interventions useless;
+            useless.day = (int)NUDday.Value;
+            useless.revenue = Convert.ToSingle(TXTrevenue.Text);
+            useless.interventionType = TXTservice.Text;
+
+            inserisci(useless);
+
+        }
+
+        public void inserisci(interventions useless)
+        {
+            leggiDati();
+
+            if (nv == MAXV)
+            {
+                MessageBox.Show("Vettore pieno");
+                return;
+            }
+
+            array[nv].day = useless.day;
+            array[nv].interventionType = useless.interventionType;
+            array[nv].revenue = useless.revenue;
+        }
+    }
+}
